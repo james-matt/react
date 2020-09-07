e0fca06b1a61f5a48f6ad2361ebcfb4b0ecc2419
james.taggart.43 <james.taggart.43@gmail.com>
2020-09-06 22:52:25 -0700

    Videos 8-9 React / Babel setup
    Configuration of react and babel. Basic project structure. Babel
    presets. Env for es6, es7. React babel preset for jsx and react
    syntax. Babel is a complier. JSX is a language extension of javascript.
    Yarn lock keeps track of all immediate dependency version and all sub
    dependency versions.
    
    React, babel, yarn, project directory structure, babel presets, reactdom.

---
+++ projects/indecision-app/public/scripts/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var template = React.createElement(
+  'h1',
+  null,
+  'Indecision App'
+);
+var app = document.getElementById('app');
+
+ReactDOM.render(template, app);
---
+++ projects/indecision-app/src/app.js
@@ -0,0 +1,4 @@
+var template = <h1>Indecision App</h1>
+var app = document.getElementById('app')
+
+ReactDOM.render(template, app)
